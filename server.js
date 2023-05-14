5a6c9fc70b4410993537a6930d80fef63d44bfef
Huntley <huntleycastner@gmail.com>
2023-05-14 08:48:30 -0700

    created remaining controllers

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 // DEPENDENCIES
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const { Sequelize } = require('sequelize');
 
 // CONFIGURATION / MIDDLEWARE
-require('dotenv').config()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+require('dotenv').config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: process.env.PG_PASSWORD
+});
 
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
-        message: 'Welcome to the Tour API'
-    })
-})
+        message: 'Welcome to the Tour API!'
+    });
+});
+
+// CONTROLLERS
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
+
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
-})
+    console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
+});
