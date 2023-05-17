10b0ea464b4cfe997e1e77df90da5357613bb9ed
Huntley <huntleycastner@gmail.com>
2023-05-16 17:24:33 -0700

    Finished'

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -1,26 +1,14 @@
 // DEPENDENCIES
 const bands = require('express').Router()
 const db = require('../models')
-const { Band } = db 
-
-// FIND ALL BANDS
-bands.get('/', async (req, res) => {
-    try {
-        const foundBands = await Band.findAll()
-        res.status(200).json(foundBands)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
-
-// DEPENDENCIES 
+const { Band, MeetGreet, Event, SetTime } = db
 const { Op } = require('sequelize')
-   
+
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
         const foundBands = await Band.findAll({
-            order: [ [ 'available_start_time', 'ASC' ] ],
+            order: [['available_start_time', 'ASC']],
             where: {
                 name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
             }
@@ -29,7 +17,35 @@ bands.get('/', async (req, res) => {
     } catch (error) {
         res.status(500).json(error)
     }
-})
+});
+
+// FIND A SPECIFIC BAND
+bands.get('/:name', async (req, res) => {
+    try {
+        const foundBand = await Band.findOne({
+            where: { band_id: req.params.name },
+            include: [
+                {
+                    model: MeetGreet,
+                    as: "meet_greets",
+                    include: { model: Event, as: "event" }
+                },
+                {
+                    model: SetTime,
+                    as: "set_times",
+                    include: {
+                        model: Event,
+                        as: "event",
+                        where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } }
+                    }
+                }
+            ]
+        })
+        res.status(200).json(foundBand)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
 
 // CREATE A BAND
 bands.post('/', async (req, res) => {
@@ -39,10 +55,10 @@ bands.post('/', async (req, res) => {
             message: 'Successfully inserted a new band',
             data: newBand
         })
-    } catch(err) {
-        res.status(500).json(err)
+    } catch (error) {
+        res.status(500).json(error)
     }
-})
+});
 
 // UPDATE A BAND
 bands.put('/:id', async (req, res) => {
@@ -55,10 +71,10 @@ bands.put('/:id', async (req, res) => {
         res.status(200).json({
             message: `Successfully updated ${updatedBands} band(s)`
         })
-    } catch(err) {
-        res.status(500).json(err)
+    } catch (error) {
+        res.status(500).json(error)
     }
-})
+});
 
 // DELETE A BAND
 bands.delete('/:id', async (req, res) => {
@@ -71,10 +87,10 @@ bands.delete('/:id', async (req, res) => {
         res.status(200).json({
             message: `Successfully deleted ${deletedBands} band(s)`
         })
-    } catch(err) {
-        res.status(500).json(err)
+    } catch (error) {
+        res.status(500).json(error)
     }
-})
+});
 
 // EXPORT
 module.exports = bands
